44ddc876ae6cb01516312bd0935b2f4ad3691822
Andrew Tan <andrewvmail@gmail.com>
2018-10-11 00:32:53 -0700

    related to last commit

--- nativescript/src/app/core/core.module.ts
+++ nativescript/src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { DataService } from "./data.service";
 
 require("nativescript-nodeify");
+require('nativescript-websockets');
 
 @NgModule({
     providers: [
