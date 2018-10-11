44e7858dbda509e46a0811aa2b1d45b70e9ef1c5
Andrew Tan <andrewvmail@gmail.com>
2018-10-11 04:05:05 -0700

    add state sequences and providers

--- nativescript/src/app/main/index.ts
+++ nativescript/src/app/main/index.ts
@@ -1,12 +1,54 @@
 import App from "cerebral";
 import Devtools from "cerebral/devtools";
+import { debounce, set } from "cerebral/factories";
+import { props, state } from "cerebral/tags";
+
+const Timer = () => {
+  let timer;
+  return context => {
+    return {
+      start(message) {
+        console.log('start timer')
+      }
+    };
+  };
+};
+
 
 const app = App(
   {
     state: {
-      hello: "world"
+      text: "Ready to run?",
+      buttonText: "Start",
+      timer: {
+        // use new Date().getTime() for these
+        start: 0,
+        stop: 0
+      },
+      settings: {
+        // in seconds
+        walk: 0,
+        run: 0,
+        sprint: 0
+      }
     },
-    providers: {}
+    sequences: {
+      start: [
+        function start({ state, timer }) {
+          state.set("timer.start", new Date().getTime());
+          timer.start();
+        }
+      ],
+      stop: [function stop() {}],
+      onSliderChange: [
+        function setSetting({ state, props }) {
+          state.set("settings." + props.set, props.value);
+        }
+      ]
+    },
+    providers: {
+      timer: Timer()
+    }
   },
   {
     devtools: Devtools({
