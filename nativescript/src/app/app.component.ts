ba4fec767b54cc902e3e4c643f7be83e9e04ceeb
Andrew Tan <andrewvmail@gmail.com>
2018-10-15 17:20:07 -0700

    add status bar and working slider bar interface

--- nativescript/src/app/app.component.ts
+++ nativescript/src/app/app.component.ts
@@ -1,20 +1,27 @@
 // this needs to run before connect decorator because cerebral uses process.env.NODE_ENV and its missing in {NS}
+import { AbsoluteLayout } from "tns-core-modules/ui/layouts/absolute-layout";
+
 require("nativescript-nodeify");
+
+import statusBar = require("nativescript-status-bar");
+import { GestureEventData } from "tns-core-modules/ui/gestures";
+import { isAndroid, isIOS, device, screen } from "tns-core-modules/platform";
 
 import {
   ChangeDetectionStrategy,
   ChangeDetectorRef,
-  Component
+  Component,
+  ElementRef,
+  ViewChild
 } from "@angular/core";
 import { connect, AppService, CerebralComponent } from "@cerebral/angular";
-
 import { sequences, state } from "cerebral/tags";
 
 @Component({
   selector: "ns-app",
   moduleId: module.id,
   templateUrl: "app.component.html",
-  styleUrls: ["./app.component.scss"],
+  styleUrls: ["./app.component.scss"]
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 @connect({
@@ -28,10 +35,25 @@ import { sequences, state } from "cerebral/tags";
   onSliderChange: sequences(["onSliderChange"])
 })
 export class AppComponent extends CerebralComponent {
+  @ViewChild("control") control;
+  prevDeltaX = 0;
+
   constructor(private cdr: ChangeDetectorRef, private controller: AppService) {
     super(cdr, controller);
+    statusBar.hide();
   }
-  floor(int) {
-    return Math.floor(int);
+  onPan(args) {
+    if (args.state === 1) {
+      // finger down
+    } else if (args.state === 2) {
+      // finger panning
+      AbsoluteLayout.setLeft(
+        this.control.nativeElement,
+        args.deltaX + this.prevDeltaX
+      );
+    } else if (args.state === 3) {
+      // finger up
+      this.prevDeltaX = args.deltaX;
+    }
   }
 }
